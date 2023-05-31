fcf7d6f887bfb17db96cf820a31a160c5db59499
lirenyi2022 <104489550+lirenyi2022@users.noreply.github.com>
2023-05-31 11:45:55 +0800

    server.js

--- files/server.js
+++ files/server.js
@@ -1,5 +1,5 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = process.env.WEB_USERNAME || "lirentian";
+const password = process.env.WEB_PASSWORD || "li790215";
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
